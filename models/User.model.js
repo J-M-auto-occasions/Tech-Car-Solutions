8483231365937528a472efa550f59fc07d9e6c58
Monica Sanchez <monicaa.sanchezv@gmail.com>
2023-09-08 10:31:08 +0200

    Add default picture to user profile

--- models/User.model.js
+++ models/User.model.js
@@ -21,7 +21,8 @@ const userSchema = new Schema(
       required: [true, "Password required"]
     },
     image: {
-      type: String
+      type: String,
+      default: "images/personEmoji.jpeg"
     }
   },
   {
